a5bf1cd9e52485e4211bf3d3ad14b93888ebbe3c
conectabell <conectabell@gmail.com>
2017-02-04 11:07:06 +0100

    Incluido editor Markdown

--- blogapp/blog/forms.py
+++ blogapp/blog/forms.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from .models import Post
+from pagedown.widgets import PagedownWidget
 
 
 class PostForm(forms.ModelForm):
-
+    text = forms.CharField(widget=PagedownWidget())
     class Meta:
         model = Post
         fields = ('title', 'text',)
