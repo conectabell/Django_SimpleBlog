834f95c54a1042ccb7a51dda95750d53bab5cc2d
conectabell <conectabell@gmail.com>
2017-02-05 23:18:57 +0100

    Cifrado AES para posts

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -43,7 +43,7 @@ def post_new(request):
 
 
 @login_required(login_url='django.contrib.auth.views.login')
-def post_edit(request, pk, form=None):
+def post_edit(request, pk, key=None):
     try:
         print "REQ---"
         print request
@@ -51,41 +51,65 @@ def post_edit(request, pk, form=None):
         timezone.now()).order_by('published_date')
         post = get_object_or_404(Post, pk=pk)
         print "POST:---" + str(post)
-
-        if post.crypt is True:
-            print "post.crypt TRUE"
-            return redirect('blog.views.key_check_edit', pk=pk)
-        else:
-            print "post.crypt FALSE"
+        #if post.crypt is True:
+            #print "post.crypt TRUE"
+            #return redirect('blog.views.key_check_edit', pk=pk)
+        #else:
+            #print "post.crypt FALSE"
         print "METHOD: " + str(request.method)
         if request.method == "POST":
-            form2 = PostForm(request.POST)
+            #form2 = PostForm(form)
+            form = PostForm(request.POST)
             #form2 = form
             #form = request.POST
-            post2 = form2.save(commit=False)
+            post = form.save(commit=False)
             print "__POST2_CRYPT: "
-            print post2.crypt
-            if form2.is_valid():
-                if post2.crypt is True:
-                    print "POST2 IS TRUE"
-                    cifr = crypt.AESCipher(post2.key)
-                    txt_enc = cifr.encrypt(post2.text)
-                    post2.text = txt_enc
-                    hash_object = hashlib.sha256(post2.key)
+            #print form.cleaned_data['crypt']
+            if form.is_valid():
+                if form.cleaned_data['crypt'] is True:
+                    print "CRYPT IS TRUE"
+                    cifr = crypt.AESCipher(form.cleaned_data['key'])
+                    txt_enc = cifr.encrypt(form.cleaned_data['text'])
+                    post.text = txt_enc
+                    hash_object = hashlib.sha256(form.cleaned_data['key'])
                     hex_dig = hash_object.hexdigest()
-                    post2.key = hex_dig
-                    post2.rekey = hex_dig
-                post2.author = request.user
-                post2.save()
-                return post_detail(request, pk=post2.pk)
+                    post.key = hex_dig
+                    post.rekey = hex_dig
+                    post.pk = pk
+                    post.author = request.user
+                    post.save()
+                    return post_detail(request, pk=pk)
+                else:
+                    print "CRYPT IS FALSE"
+                    post.pk = pk
+                    post.author = request.user
+                    print "post PK: " + post.pk
+                    try:
+                        if post.save():
+                            print "post supuestamente salvado"
+                        else:
+                            print "aqui ha pasado algo, en el save"
+                    except Exception as e:
+                        print "ERROR_SAVE: " + str(e)
+                    return post_detail(request, pk=pk)
             else:
-                print "Formulario No Valido"
+                print "Post-Edit: Formulario No Valido"
                 return render(request, 'blog/post_edit.html', {'form': form, 'posts': posts})
         else:
-            print "Paso por ELSE"
-            form = PostForm(instance=post)
-        #return redirect('blog.views.post_edit', pk=post.pk, form=)
-        return render(request, 'blog/post_edit.html', {'form': form, 'posts': posts},)
+            print "Post-Edit: No es un metodo POST- Devuelve Formulario con post extraido de la pk"
+            if post.crypt is True:
+                if request.session['keytmp']:
+                    print "KEYTMP: " + request.session['keytmp']
+                else:
+                    print "KEYTMP: NO HAY"
+                cifr = crypt.AESCipher(request.session['keytmp'])
+                txt_dec = cifr.decrypt(post.text)
+                post.text = txt_dec
+                form = PostForm(instance=post)
+            else:
+                form = PostForm(instance=post)
+            #return redirect('blog.views.post_edit', pk=post.pk, form=)
+            return render(request, 'blog/post_edit.html', {'form': form, 'posts': posts},)
     except Exception as e:
         print "ERROR post_edit: " + str(e)
 
@@ -124,6 +148,7 @@ def key_check(request, pk):
                 cifr = crypt.AESCipher(p)
                 txt_dec = cifr.decrypt(post.text)
                 post.text = txt_dec
+                request.session['keytmp'] = p
                 return render(request, 'blog/post_detail.html', {'post': post ,
                                                     'posts': posts})
     form = KeyCheckForm()
@@ -160,10 +185,11 @@ def key_check_edit(request, pk):
                     form2.text = txt_dec
                     form2.crypt = False
                     print form2
+                    request.session['keytmp'] = p
                     #form2.save()
                     #return post_edit(request, pk)
-                    #return redirect('blog.views.post_edit', pk=pk, form=form2, context=RequestContext(request))
-                    #return post_edit(pk=post.pk, form=form2)
+                    #return redirect('blog.views.post_edit', pk=pk, key=p)
+                    #return post_edit(request, pk=post.pk, key=p)
                     return render(request, 'blog/post_edit.html', {'form': form2, 'posts': posts})
                     #return post_edit(request, )
         form = KeyCheckForm()
